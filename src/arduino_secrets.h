7409b3035a7b0b509da4f1c0bfe11f9c2fc6a8c4
harutiun <harut.alepogian@gmail.com>
2020-02-01 12:15:05 +0100

    version 31.01 subido a todos los sensores del colegio

---
+++ src/arduino_secrets.h
@@ -0,0 +1,14 @@
+//#define SECRET_SSID "Arbeitsraum"
+//#define SECRET_PASS "patoAnda01_!"
+
+#define SECRET_SSID "Sensor"
+#define SECRET_PASS "WmA85RpRu26ZpPDp"
+
+//#define SECRET_SSID "BYOD"
+//#define SECRET_PASS "Alberto1516"
+
+//#define SECRET_SSID "MOVISTAR_6DA7"
+//#define SECRET_PASS "6A375011BB6F5064A1C8"
+
+#define MQTT_USER "admin"
+#define MQTT_PASS "admin"
