d3dac370ffedebfda2b1669737a6e5e1c4896805
Harutiun <harut.alepoglian@gmail.com>
2020-02-02 21:00:19 +0100

    Update arduino_secrets.h

--- src/arduino_secrets.h
+++ src/arduino_secrets.h
@@ -1,11 +1,5 @@
-//#define SECRET_SSID "Arbeitsraum"
-//#define SECRET_PASS "patoAnda01_!"
+#define SECRET_SSID "YOUR SSID"
+#define SECRET_PASS "YOUR_SECRET_PASSWORD"
 
-#define SECRET_SSID "Sensor"
-#define SECRET_PASS ""
-
-//#define SECRET_SSID "MOVISTAR_6DA7"
-//#define SECRET_PASS "6A375011BB6F5064A1C8"
-
-#define MQTT_USER "admin"
-#define MQTT_PASS ""
+#define MQTT_USER "YOUR_MQTT_USER"
+#define MQTT_PASS "YOUR_MQTT_PASSWORD"
